2eaa45dd553ba3d4a345136b5791dfc31a88810f
shlokburmi <135860207+shlokburmi@users.noreply.github.com>
2026-02-22 16:12:00 +0530

    136. Single Number solution

---
+++ Apna College DSA Sheet/vector.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> vec{};
+
+
+    // cout<<"Size of vector is: "<<vec.size()<<endl; //size of vector is 0 as it is empty
+
+
+    // vec.push_back(25); // adds an element at the end of the vector
+    // cout<<"Size of vector is: "<<vec.size()<<endl; //size of vector is 1 as we have added an element in the vector
+
+    // vec.pop_back(); //removes the last element
+    // cout<<"Size of vector is: "<<vec.size()<<endl; //size of vector is 0 as we have removed the only element present in the vector
+    
+    
+    
+    // cout<<vec.front()<<endl; //gives the first element of the vector
+    // cout<<vec.back()<<endl; //gives the last element of the vector
+    // cout<<vec.at(0)<<endl; //gives the element at index 0 of the vector
+
+    
+
+
+    return 0;
+
+}
