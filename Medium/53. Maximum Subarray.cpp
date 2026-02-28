50eb486a4ef9f529ec945e9759f559a61850e2a2
shlokburmi <135860207+shlokburmi@users.noreply.github.com>
2026-02-28 21:29:25 +0530

    small changes

--- Medium/53. Maximum Subarray.cpp
+++ Medium/53. Maximum Subarray.cpp
@@ -7,8 +7,8 @@ public:
         int currSum=0,maxSum=INT_MIN;
         int n=nums.size();
 
-        for(int i=0;i<n;i++){
-            currSum += nums[i];
+        for(int val:nums){
+            currSum += val;
             maxSum =max(currSum,maxSum);
             if( currSum<0){
                 currSum=0;
