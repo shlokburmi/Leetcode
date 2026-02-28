f26a7055728c8570e605a0491c1145266d359b6d
shlokburmi <135860207+shlokburmi@users.noreply.github.com>
2026-02-28 21:28:22 +0530

    53. Maximum Subarray Kadanes Algorithm

---
+++ Medium/53. Maximum Subarray.cpp
@@ -0,0 +1,20 @@
+// KADANE'S ALGORITHM
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currSum=0,maxSum=INT_MIN;
+        int n=nums.size();
+
+        for(int i=0;i<n;i++){
+            currSum += nums[i];
+            maxSum =max(currSum,maxSum);
+            if( currSum<0){
+                currSum=0;
+            }
+        }
+        return maxSum;
+        
+    }
+};
