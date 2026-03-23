bb39713e9de54bcde2e4b6d0746b6069d8004f78
shlokburmi <135860207+shlokburmi@users.noreply.github.com>
2026-03-23 23:01:19 +0530

    189

---
+++ Medium/189. Rotate Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k = k % n;
+        vector<int> temp(k);
+        for (int i=0;i<k;i++){
+            temp[i]=nums[n-k+i];
+        }
+        for (int i=n-k-1;i>=0;i--){
+            nums[i+k]=nums[i];
+        }
+        for (int i=0;i<k;i++){
+            nums[i]=temp[i];
+        }
+    }
+};
