abc9c2393b44d60de8be613c5f1146e01b2f9869
shlokburmi <135860207+shlokburmi@users.noreply.github.com>
2026-06-09 18:22:25 +0530

    3689

---
+++ Daily Quest/3689. Maximum Total Subarray Value I.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        int n = nums.size();
+        int maxEl = INT_MIN;
+        int minEl = INT_MAX;
+
+
+        for(int &num:nums){
+            maxEl=max(maxEl,num);
+            minEl=min(minEl,num);
+        }
+        return 1L*k*(maxEl-minEl);
+
+        
+    }
+};
